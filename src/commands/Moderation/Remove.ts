2bfbf92cca676a6918e930794e9b9e02c99f8e2f
ErMurazor-Tech <76946507+ErMurazor-Tech@users.noreply.github.com>
2021-09-29 22:29:37 +0530

    😕❤Queen Hesh- by Er Murazor❤😕

--- src/commands/Moderation/Remove.ts
+++ src/commands/Moderation/Remove.ts
@@ -9,7 +9,7 @@ export default class Command extends BaseCommand {
             adminOnly: true,
             aliases: ['boom', 'kick'],
             command: 'remove',
-            description: 'removes the mentioned users',
+            description: 'ඔබ සඳහන් කරන පරිශීලකයන්ව සමූහයෙන් ඉවත් කරයි',
             category: 'moderation',
             usage: `${client.config.prefix}remove [@mention | tag]`,
             baseXp: 10
@@ -18,16 +18,16 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
-            return void M.reply(`❌ Failed to ${this.config.command} as I'm not an admin`)
+            return void M.reply(`❌ මම ඇඩ්මින් වරයෙක් නොවන නිසාඅ ${this.config.command} විධානය ඉටු කිරීමට නොහැකි විය`)
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
-        if (!M.mentioned.length) return void M.reply(`Please tag the users you want to ${this.config.command}`)
+        if (!M.mentioned.length) return void M.reply(`ඔබට ${this.config.command} කිරීමට අවශ්‍ය පරිශීලකයන්ව සඳහන් කරන්න`)
         M.mentioned.forEach(async (user) => {
             const usr = this.client.contacts[user]
             const username = usr.notify || usr.vname || usr.name || user.split('@')[0]
-            if (M.groupMetadata?.admins?.includes(user)) M.reply(`❌ Skipped *${username}* as they're an admin`)
+            if (M.groupMetadata?.admins?.includes(user)) M.reply(`❌ *${username}* ඇඩ්මින් කෙනෙකු වේ`)
             else {
                 await this.client.groupRemove(M.from, [user])
-                M.reply(`🏌️‍♂️Successfully Removed *${username}*`)
+                M.reply(`🏌️‍♂️සාර්ථකව ඉවත් කෙරිණි *${username}*`)
             }
         })
     }
