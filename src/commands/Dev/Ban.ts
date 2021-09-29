4ab48bfc52358dded844c7daf0285f27645009c4
ErMurazor-Tech <76946507+ErMurazor-Tech@users.noreply.github.com>
2021-09-29 20:39:54 +0530

    😕❤Queen Hesh- by Er Murazor❤😕

--- src/commands/Dev/Ban.ts
+++ src/commands/Dev/Ban.ts
@@ -7,17 +7,17 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'ban',
-            description: 'Bans the tagged users',
+            description: 'ඔබ විසින් tag කල පුද්ගලයාව සමූහයෙන් තහනම් (ban) කරයි.',
             category: 'dev',
             usage: `${client.config.prefix}ban [@tag]`
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        if (!this.client.config.mods?.includes(M.sender.jid)) return void M.reply('❌ Only available to MODS')
+        if (!this.client.config.mods?.includes(M.sender.jid)) return void M.reply('❌ සමූහයෙහි ඇඩ්මින් වරුන්ට පමණයි භාවිතා කල හැක')
         const immortals = [M.sender, this.client.user.jid]
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
-        if (!M.mentioned.length || !M.mentioned[0]) return void M.reply('Mention the user whom you want to ban')
+        if (!M.mentioned.length || !M.mentioned[0]) return void M.reply('ඔබට ban කිරීමට අවශ්‍ය පුද්ගලයාව mention කරන්න')
         let text = '*STATE*\n\n'
         for (const user of M.mentioned) {
             if (immortals.includes(user)) continue
@@ -25,11 +25,11 @@ export default class Command extends BaseCommand {
             const info = this.client.getContact(user)
             const username = info.notify || info.vname || info.name || user.split('@')[0]
             if (data?.ban) {
-                text += `🟨 ${username}: Already Banned\n`
+                text += `🟨 ${username}: දැනටමත් තනමට ලක්වී ඇත.\n`
                 continue
             }
             await this.client.banUser(user)
-            text += `🟥 ${username}: Banned\n`
+            text += `🟥 ${username}: තහනමට ලක් විය.\n`
         }
         M.reply(text)
     }
