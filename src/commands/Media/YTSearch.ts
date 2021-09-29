67344b492c92843bc93fc2889163295aa0529b10
ErMurazor-Tech <76946507+ErMurazor-Tech@users.noreply.github.com>
2021-09-29 21:52:38 +0530

    😕❤Queen Hesh- by Er Murazor❤😕

--- src/commands/Media/YTSearch.ts
+++ src/commands/Media/YTSearch.ts
@@ -9,7 +9,7 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'yts',
-            description: 'Searches YT',
+            description: 'YouTube හි සෙවීමට',
             category: 'media',
             aliases: ['ytsearch'],
             usage: `${client.config.prefix}yts [term]`,
@@ -18,31 +18,31 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-        if (!joined) return void M.reply('🔎 Provide a search term')
+        if (!joined) return void M.reply('🔎 සෙවීමට අවශ්‍ය වචනය ලබා දෙන්න')
         const term = joined.trim()
         const { videos } = await yts(term)
-        if (!videos || videos.length <= 0) return void M.reply(`⚓ No Matching videos found for : *${term}*`)
+        if (!videos || videos.length <= 0) return void M.reply(`⚓ කිසිඳු ගැලපෙන වීඩියෝවක් හමුවූයේ නැත : *${term}*`)
         const length = videos.length < 10 ? videos.length : 10
-        let text = `🔎 *Results for ${term}*\n`
+        let text = `🔎 *ප්‍රතිපල ලබාදීම ${term}*\n`
         for (let i = 0; i < length; i++) {
-            text += `*#${i + 1}*\n📗 *Title:* ${videos[i].title}\n📕 *Channel:* ${
+            text += `*#${i + 1}*\n📗 *තේමාව:* ${videos[i].title}\n📕 *නාලිකාව:* ${
                 videos[i].author.name
-            }\n 📙 *Duration:* ${videos[i].duration}\n📘 *URL:* ${videos[i].url}\n\n`
+            }\n 📙 *ධාවන කාලය:* ${videos[i].duration}\n📘 *සබැඳිය:* ${videos[i].url}\n\n`
         }
-        M.reply('👾 searching...')
+        M.reply('❤ සොයමින් පවතී')
         this.client
             .sendMessage(M.from, text, MessageType.extendedText, {
                 quoted: M.WAMessage,
                 contextInfo: {
                     externalAdReply: {
-                        title: `Search Term: ${term}`,
-                        body: `❤Handcrafted for you by Queen Hesh❤`,
+                        title: `සෙවුම් වචනය: ${term}`,
+                        body: `❤Queen Hesh වෙතින්❤`,
                         mediaType: 2,
                         thumbnailUrl: videos[0].thumbnail,
                         mediaUrl: videos[0].url
                     }
                 }
             })
-            .catch((reason: any) => M.reply(`❌ an error occurred, Reason: ${reason}`))
+            .catch((reason: any) => M.reply(`❌ දෝෂයක් හටගැණුනි, හේතුව: ${reason}`))
     }
 }
