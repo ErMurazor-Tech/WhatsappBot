789f4ead198ec2f3312bf64c8e4839769bbce843
ErMurazor-Tech <76946507+ErMurazor-Tech@users.noreply.github.com>
2021-09-29 22:19:09 +0530

    😕❤Queen Hesh- by Er Murazor❤😕

--- src/commands/Moderation/Demote.ts
+++ src/commands/Moderation/Demote.ts
@@ -8,7 +8,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             adminOnly: true,
             command: 'demote',
-            description: 'demotes the mentioned users',
+            description: 'ඔබ සඳහන් කරන ලද පරිශීලකයාගේ තනතුර ඉවත් කරයි',
             category: 'moderation',
             usage: `${client.config.prefix}demote [mention | @tag]`,
             baseXp: 10
@@ -17,16 +17,16 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
-            return void M.reply(`❌ Failed to ${this.config.command} as I'm not an admin`)
+            return void M.reply(`❌ මම ඇඩ්මින් වරයෙක් නොවන නිසා ${this.config.command} මෙම විධානය ඉටු කිරිමට නොහැක`)
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
-        if (!M.mentioned.length) return void M.reply(`Please tag the users you want to ${this.config.command}`)
+        if (!M.mentioned.length) return void M.reply(`ඔබට ${this.config.command} කිරීමට අවශ්‍ය පරිශීලකයාව සඳහන් කරන්න`)
         M.mentioned.forEach(async (user) => {
             const usr = this.client.contacts[user]
             const username = usr.notify || usr.vname || usr.name || user.split('@')[0]
-            if (!M.groupMetadata?.admins?.includes(user)) M.reply(`❌ Skipped *${username}* as they're not an admin`)
+            if (!M.groupMetadata?.admins?.includes(user)) M.reply(`❌ *${username}* ඇඩ්මින් වරයෙකු නොවේ`)
             else if (user !== this.client.user.jid) {
                 await this.client.groupDemoteAdmin(M.from, [user])
-                M.reply(`➰ Successfully Demoted *${username}*`)
+                M.reply(`➰ සාර්ථකව තනතුරෙන් පහකරන ලදී *${username}*`)
             }
         })
     }
