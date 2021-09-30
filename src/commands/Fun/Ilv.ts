5c674ba3c05a0128dee27dd7c5b8ac63c6225556
ErMurazor-Tech <76946507+ErMurazor-Tech@users.noreply.github.com>
2021-09-30 22:06:07 +0530

    😕❤Queen Hesh- by Er Murazor❤😕

---
+++ src/commands/Fun/Ilv.ts
@@ -0,0 +1,19 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'ilv',
+            description: 'ilv යන්නෙහි අරුත I Love You යන්නයි. විධානය යවා බලන්න...',
+            category: 'misc',
+            usage: `${client.config.prefix}ilv`,
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        return void (await M.reply(`I Love too... ${M.sender.username}!❤❤`))
+    }
+}
