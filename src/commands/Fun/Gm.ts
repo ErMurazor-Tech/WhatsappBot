1c88e391611b207bda4f5f77de119974fa4a2102
ErMurazor-Tech <76946507+ErMurazor-Tech@users.noreply.github.com>
2021-10-01 09:08:52 +0530

    😕❤Queen Hesh- by Er Murazor❤😕

--- src/commands/Fun/Gm.ts
+++ src/commands/Fun/Gm.ts
@@ -16,12 +16,12 @@ export default class Command extends BaseCommand {
     }
     run = async (M:ISimplifiedMessage) : Promise<void> => {
         const user1 = M.sender.jid
-        const user2 = M.mentioned(0)
+        const user2 = M.mentioned[0]
 
         const n = ['./assets/images/gm1.jpg', './assets/images/gm2.jpg', './assets/images/gm3.jpg', './assets/images/gm4.jpg',]
         let hug = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, {url : hug},
             MessageType.image,
-            {mimetype : Mimetype.jpg, caption : `ඔයාට සුබම සුබ උදෑසනක් වේවා ${M.mentioned(0).username}!❤❤`})
+            {mimetype : Mimetype.jpeg, caption : `ඔයාට සුබම සුබ උදෑසනක් වේවා ${M.mentioned[0].username}!❤❤`})
     }
 }
