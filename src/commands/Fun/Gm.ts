d363e6b5bb9b72d818a0ae9e4fc1f19bb297bc00
ErMurazor-Tech <76946507+ErMurazor-Tech@users.noreply.github.com>
2021-10-01 10:10:54 +0530

    😕❤Queen Hesh- by Er Murazor❤😕

--- src/commands/Fun/Gm.ts
+++ src/commands/Fun/Gm.ts
@@ -15,10 +15,19 @@ export default class Command extends BaseCommand {
         })
     }
     run = async (M:ISimplifiedMessage) : Promise<void> => {
+        const immortals = [M.sender, this.client.user.jid]
+        if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
+        if (!M.mentioned.length || !M.mentioned[0]) return void M.reply('සුබපැතීමට අවශ්‍ය පුද්ගලයාව සඳහන් කරන්න')
+        let text = '*STATE*\n\n'
+        for (const user of M.mentioned) {
+            if (immortals.includes(user)) continue
+            const data = await this.client.getUser(user)
+            const info = this.client.getContact(user)
+            const username = info.notify || info.vname || info.name || user.split('@')[0]
         const n = ['./assets/images/gm1.jpg', './assets/images/gm2.jpg', './assets/images/gm3.jpg', './assets/images/gm4.jpg',]
         let hug = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, {url : hug},
             MessageType.image,
-            {mimetype: Mimetype.jpeg, caption: `ඔයාට සුබම සුබ උදෑසනක් වේවා!❤❤`})
+            {mimetype: Mimetype.jpeg, caption: `ඔයාට සුබම සුබ උදෑසනක් වේවා ${username}!❤❤`})
     }
 }
