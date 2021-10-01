3d59e8eb1e797d04850cf01e2a3b414f1eedf3eb
ErMurazor-Tech <76946507+ErMurazor-Tech@users.noreply.github.com>
2021-10-01 08:43:38 +0530

    😕❤Queen Hesh- by Er Murazor❤😕

---
+++ src/commands/Fun/Gm.ts
@@ -0,0 +1,28 @@
+import { messagetype, MimeType } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import request from '../../lib/request'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import Command from '../_Command_Example'
+
+export default class Command extends BaseCommand {
+    constructor (client: WAClient , handler: MessageHandler) {
+        super(client, handler, {
+            command : 'gm',
+            description : 'ඔබ සඳහන් කල පුද්ගලයන් වෙත සුභ උදෑසනක් පතයි',
+            category : 'fun',
+            usage : `${client.config.prefix}gm@<user>`,
+        })
+    }
+    run = async (M:ISimplifiedMessage) : Promise<void> => {
+        const user1 = M.sender.jid
+        const user2 = M.mentioned(0)
+
+        const n = ['./assets/images/gm1.jpg', './assets/images/gm1.jpg', './assets/images/gm1.jpg', './assets/images/gm1.jpg',]
+        let hug = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, {url : hug},
+            messagetype.image
+            {mimetype : MimeType.jpg, caption : `ඔයාට සුබම සුබ උදෑසනක් වේවා ${M.mentioned.username}!❤❤`})
+    }
+}
